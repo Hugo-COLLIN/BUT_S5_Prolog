ee825d72c1b21617f06232c441e00c958fd01175
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-12-13 11:59:46 +0100

    ex3 solution

--- canards.pl
+++ canards.pl
@@ -1,6 +1,21 @@
-appartient_a().
-marque_avec().
-pas_gris().
-vit_dans().
-canard().
-porte_col_dentelle().
+appartient_a(X, mrs_bond) :-
+    canard(X),
+    vit_dans(X, ce_village),
+    marque_avec(X, "B").
+
+appartient_a(X, mrs_bond) :-
+    canard(X),
+    vit_dans(X, ce_village),
+    porte_col_dentelle(X).
+pas_gris(X) :-
+    canard(X),
+    vit_dans(X, ce_village),
+    appartient_a(X, mrs_bond).
+
+canard(george).
+vit_dans(george, ce_village).
+marque_avec(george, "AB").
+porte_col_dentelle(george).
+canard(augusta).
+vit_dans(augusta, ce_village).
+marque_avec(augusta, "B").
